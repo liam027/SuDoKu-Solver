ceb8f61e19c61d44f00af72dd6f31d78d24c6089
Liam027 <liam027@gmail.com>
2019-08-23 08:07:01 -0400

    cleaned up comments

--- App.py
+++ App.py
@@ -1,54 +1,43 @@
+from SDKCell import *
+from SDKGrid import *
+import Solver
 from tkinter import filedialog
 from tkinter import *
-from SDKGrid import *
-from SDKCell import *
-import Solver
 
 class App:
-
     def __init__(self, master, **kwargs):
-        #generate new SDKgrid for future input value storage
         self.puzzleGrid = SDKGrid("")
         self.solver = Solver.Solver()
 
-        #window size, spacer and container for entry elements
         self.master=master
         master.geometry('550x600+0+0')
+
         top_title_header = Frame(self.master,width = 600, height = 50)
         top_title_header.pack()
-
-        #botTitleHeader = Frame(self.master,width = 600, height = 10)
-        #botTitleHeader.pack()
-
         top_frame = Frame(self.master,width = 600, height = 600)
         top_frame.pack()
-
-        #create input elements and assign to SDKgrid
+        #create input (entry) widgets and assign to SDKgrid
         for x in range(9):
             for y in range(9):
                 input = Entry(top_frame, width = 2, font = ("Courier", 24), justify="center")
-                input.cell = self.puzzleGrid.get_cell_at_coords(x,y)
                 self.puzzleGrid.get_cell_at_coords(x,y).entryBox = input
                 input.grid(row=x,column=y)
 
-        #separate grid and buttons
         grid_to_label_spacer = Frame(width = 600, height = 10)
         grid_to_label_spacer.pack()
-
         #label for messages
         self.message = StringVar()
         self.message.set("LOAD or manually input a puzzle!")
         self.label_for_messages = Label(self.master, textvariable = self.message)
         self.label_for_messages.pack()
-
         #separate grid and buttons
         label_to_button_spacer = Frame(width = 600, height = 10)
         label_to_button_spacer.pack()
-
         #container for buttons
         bottom_frame = Frame( width = 600, height = 50)
         bottom_frame.pack()
 
+    """Button Widgets"""
         solve_button = Button(bottom_frame, text = "SOLVE", fg = "blue", command=lambda : self.solve_button())
         solve_button.pack(side=LEFT)
 
@@ -64,21 +53,23 @@ class App:
         quit_button = Button(bottom_frame, text = "QUIT", fg = "red", command=bottom_frame.quit)
         quit_button.pack(side=LEFT)
 
-        """Button methods"""
+    """Button methods"""
     def solve_button(self):
+        """solves the entire puzzle"""
         if self.puzzleGrid.assign_input_values():
-            for i in range(30):
+            for i in range(30): #[NOTE] num of iterations can be tweaked
                 for x in range(9):
                     for y in range(9):
                         self.solver.solve_full(self.puzzleGrid,x,y)
         else:
-            print("Inputs not valid!")
+            print("Bad Input! Make sure you've entered only single digits!")
         if self.isComplete() == True:
             print("Puzzle Completed!")
         else:
-            print("Puzzle NOT Complete!")
+            print("Puzzle solving FAILED!")
 
     def step_button(self):
+        """solves one cell of the the puzzle"""
         self.foundStep = False
         if self.puzzleGrid.assign_input_values():
             for x in range(9):
@@ -86,11 +77,11 @@ class App:
                     if self.foundStep == False:
                         if(self.solver.solve_step(self.puzzleGrid,x,y)):
                             self.foundStep = True
-
         if self.isComplete() == True:
             self.message.set("Puzzle completed!")
 
     def save(self):
+        """saves the current input as a .json puzzle"""
         if self.puzzleGrid.assign_input_values():
             fout = filedialog.asksaveasfilename(initialdir="puzzles/",title="Select save location", filetypes =(("JSON files","*.json"),("All file types","*.*")))
             if isinstance(fout, str):
@@ -99,6 +90,7 @@ class App:
             self.message.set("Bad Input! Make sure you've entered only single digits!")
 
     def load(self,file = ""):
+        """load a .json puzzle"""
         self.puzzleGrid.clear()
         if file == "":
             file = filedialog.askopenfilename(initialdir="puzzles/",title="Select file", filetypes =(("JSON files","*.json"),("All file types","*.*")))
@@ -106,7 +98,7 @@ class App:
             self.puzzleGrid.load(file)
             self.message.set("SOLVE the puzzle or STEP through the solution!")
 
-    def show_remaining_cells_possibilities(self):
+    def print_remaining_cells_possibilities(self):
         for x in range(9):
             for y in range(9):
                 cell = self.puzzleGrid.grid[x][y]
@@ -114,6 +106,7 @@ class App:
                     print(f'@{x},{y}: {cell.possibilities}')
 
     def isComplete(self):
+        """check if puzzle is complete"""
         for i in range(9):
             for n in range(9):
                 if self.puzzleGrid.grid[i][n].isSolved:
